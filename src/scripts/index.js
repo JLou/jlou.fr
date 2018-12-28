357c82197d8d2cdb6bf158ea5ce26c96c949b45e
Jean-Lou PIERME <jeanlou.pierme@axa.fr>
2018-12-28 12:30:34 +0100

    ajout de la navigation interactive

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,10 @@
 import $, { easing as _easing } from "jquery";
 import "jquery-circle-progress";
 import WOW from "wow.js";
+import chartHandler from "./charts";
+window.jQuery = $;
+require("waypoints/lib/jquery.waypoints");
 
-console.log("works");
 $(function() {
   _easing.easeOutBounce = function(x, t, b, c, d) {
     if ((t /= d) < 1 / 2.75) {
@@ -15,57 +17,92 @@ $(function() {
       return c * (7.5625 * (t -= 2.625 / 2.75) * t + 0.984375) + b;
     }
   };
-  let f = box => {
-    if (box.classList.contains("chart")) {
-      var chart = $(box);
 
-      let delay = Number((box.dataset.wowDelay || "0s").slice(0, -1)) * 1000;
-      setTimeout(() => {
-        chart
-          .circleProgress({
-            startAngle: -Math.PI / 2,
-            animation: { duration: 3000, easing: "easeOutBounce" },
-            fill: { gradient: ["#ff6600", "#CE534D"] },
-            size: 150
-          })
-          .on("circle-animation-progress", function(
-            event,
-            progress,
-            stepValue
-          ) {
-            $(this)
-              .find("strong")
-              .html(Math.round(100 * stepValue) + "<em>%</em>");
-          });
-      }, delay);
-    }
-  };
+  $("a").on("click", function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+
+  // let f = box => {
+  //   if (box.classList.contains("chart")) {
+  //     var chart = $(box);
+
+  //     let delay = Number((box.dataset.wowDelay || "0s").slice(0, -1)) * 1000;
+  //     setTimeout(() => {
+  //       chart
+  //         .circleProgress({
+  //           startAngle: -Math.PI / 2,
+  //           animation: { duration: 3000, easing: "easeOutBounce" },
+  //           fill: { gradient: ["#ff6600", "#CE534D"] },
+  //           size: 150
+  //         })
+  //         .on("circle-animation-progress", function(
+  //           event,
+  //           progress,
+  //           stepValue
+  //         ) {
+  //           $(this)
+  //             .find("strong")
+  //             .html(Math.round(100 * stepValue) + "<em>%</em>");
+  //         });
+  //     }, delay);
+  //   }
+  // };
 
   new WOW({
-    callback: f
+    callback: chartHandler
   }).init();
 
-  //Chart animation
-  // var circles = $(".chart");
+  $("section, header, footer").waypoint(
+    function(direction) {
+      var activeSection = $(this.element);
+      if (direction === "up") {
+        activeSection = $(this.element).prev();
+      }
 
-  // circles.appear({ force_process: true });
+      var sectionId = activeSection[0].id;
+      $(".nav__link").removeClass("active");
+      let el = $('.nav__link[href="#' + sectionId + '"]');
+      el.addClass("active");
+    },
+    {
+      offset: "25%"
+    }
+  );
 
-  // circles.on("appear", function() {
-  //   var circle = $(this);
-  //   if (!circle.data("inited")) {
-  //     circle
-  //       .circleProgress({
-  //         startAngle: -Math.PI / 2,
-  //         animation: { duration: 3000, easing: "easeOutBounce" },
-  //         fill: { gradient: ["#ff6600", "#CE534D"] },
-  //         size: 150
-  //       })
-  //       .on("circle-animation-progress", function(event, progress, stepValue) {
-  //         $(this)
-  //           .find("strong")
-  //           .html(Math.round(100 * stepValue) + "<em>%</em>");
-  //       });
-  //     circle.data("inited", true);
-  //   }
-  // });
+  $("footer").waypoint(
+    function(direction) {
+      var activeSection = $(this.element);
+      if (direction === "up") {
+        activeSection = $(this.element).prev();
+      }
+
+      var sectionId = activeSection[0].id;
+      $(".nav__link").removeClass("active");
+      let el = $('.nav__link[href="#' + sectionId + '"]');
+      el.addClass("active");
+    },
+    {
+      offset: "75%"
+    }
+  );
 });
