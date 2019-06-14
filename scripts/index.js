a7bf18e467f76459877ceb1ed6c4ec263ab4806d
Jean-Lou PIERME <jeanlou.pierme@gmail.com>
2019-06-14 09:38:44 +0200

    remove duplicate easing function

--- scripts/index.js
+++ scripts/index.js
@@ -6,18 +6,6 @@ window.jQuery = $;
 require("waypoints/lib/jquery.waypoints");
 
 $(function() {
-  _easing.easeOutBounce = function(x, t, b, c, d) {
-    if ((t /= d) < 1 / 2.75) {
-      return c * (7.5625 * t * t) + b;
-    } else if (t < 2 / 2.75) {
-      return c * (7.5625 * (t -= 1.5 / 2.75) * t + 0.75) + b;
-    } else if (t < 2.5 / 2.75) {
-      return c * (7.5625 * (t -= 2.25 / 2.75) * t + 0.9375) + b;
-    } else {
-      return c * (7.5625 * (t -= 2.625 / 2.75) * t + 0.984375) + b;
-    }
-  };
-
   $("a").on("click", function(event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
