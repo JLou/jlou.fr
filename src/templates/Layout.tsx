e84ed09ccd0f021624fa9bcb63c890ff65a453b6
Jean-Lou PIERME <jeanlou.pierme@gmail.com>
2022-01-26 22:06:05 +0100

    le menu est responsive

--- src/templates/Layout.tsx
+++ src/templates/Layout.tsx
@@ -4,15 +4,21 @@ const Layout = ({ children }) => {
   return (
     <>
       <header className="navbar">
-        <h1>Jean-Lou Piermé</h1>
+        <div className="mobile-header">
+          <h1>Jean-Lou Piermé</h1>
+          <label className="menu-toggle-label" htmlFor="menu-toggle">
+            ☰
+          </label>
+        </div>
+        <input id="menu-toggle" defaultChecked type="checkbox" />
         <nav className="nav-links">
           <a className="nav-link" href="/">
             Home
           </a>
-          <a className="nav-link" href="games">
+          <a className="nav-link" href="/games">
             Mes jeux
           </a>
-          <a className="nav-link" href="games">
+          <a className="nav-link" href="/games">
             Mes bouquins
           </a>
         </nav>
